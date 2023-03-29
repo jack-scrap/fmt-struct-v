9c3ae03f237bb17c3dd496109e92964244486e08
Jack <jackhasakeyboard@gmail.com>
2023-03-28 23:37:13 -0600

    Handled a variable number number of arguments, defaulting to having no lower versions

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	if (argc != 1 + 1 + 1 + 1) {
+	if (argc > 1 + 1 + 1 + 1) {
 		err("Wrong number of arguments");
 
 		return 1;
@@ -22,13 +22,19 @@ int main(int argc, char* argv[]) {
 
 	char* maj = argv[1];
 
-	char* min = argv[2];
+	char* min = "0";
+	if (argc > 1 + 1) {
+		min = argv[2];
+	}
 
 	if (strlen(min) > 1) {
 		err("Length of minor version too long");
 	}
 
-	char* patch = argv[3];
+	char* patch = "0";
+	if (argc > 1 + 1 + 1) {
+		patch = argv[2];
+	}
 
 	if (strlen(patch) > 1) {
 		err("Length of patch version too long");
