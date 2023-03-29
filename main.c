8ed86bbc777aecea65e9072d5df4c87d5f61bb9a
Jack <jackhasakeyboard@gmail.com>
2023-03-28 23:25:30 -0600

    Checked number of arguments, taking major, minor and patch versions

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
+#include <stdio.h>
+
+void err(char* msg) {
+	printf("Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		err("No arguments");
+
+		return 1;
+	}
+
+	if (argc != 1 + 1 + 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
 	return 0;
 }
